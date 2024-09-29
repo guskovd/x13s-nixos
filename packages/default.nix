8dab3989b865c120ab6b072437b3788a0939363d
Gian-Reto Tarnutzer <hi@giantarnutzer.com>
2024-09-29 23:03:47 +0200

    jhovold: 6.11.0-rc1 -> 6.11.0

--- packages/default.nix
+++ packages/default.nix
@@ -17,7 +17,7 @@ in
 {
   linux_jhovold = pkgs.callPackage linux_x13s_pkg {
     src = sources.linux-jhovold;
-    version = "6.11.0-rc1";
+    version = "6.11.0";
     defconfig = "johan_defconfig";
   };
 
